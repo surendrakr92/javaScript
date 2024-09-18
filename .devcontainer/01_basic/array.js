104e676b9ed26f4f97f8907982b8c4cc6c8f4727
surendrakr92 <121003012+surendrakr92@users.noreply.github.com>
2024-09-18 06:02:53 +0000

    object and arrow update

--- .devcontainer/01_basic/array.js
+++ .devcontainer/01_basic/array.js
@@ -1,6 +1,22 @@
-let newarry = ['ram','dhyam']
+let newarry = ['ram','dhyam',['aman','kishor', 'mohan'],'got','munme']
 let newarryOne = ['aman','kishor', 'mohan']
 
- newarry.push(newarryOne)
-console.log(newarry)
+//  newarry.push(newarryOne)
+// console.log(newarry)
+
+let allArray = [1,2,4, [4,5,8], [0,7,6],[34,56,67]]
+
+const conNewatrray = newarry.flat(1)
+
+// console.log(conNewatrray)
+
+// console.log(Array.from('surendra'))
+
+let scrore = 200
+let scrore2 = 400
+let scrore3 = 500
+
+console.log(Array.of(scrore,scrore2,scrore3))
+
+
 
---
+++ .devcontainer/01_basic/object.js
@@ -0,0 +1,79 @@
+
+// let newobject = {
+
+//     name:'surendra',
+//     age:'34',
+//     email:'surendra@google.com',
+//     'full name':'surendra kumar sharma',
+//     time:['monday','tusday','wednesday','thusday','friday']
+// }
+
+// // console.log(newobject["full name"])
+// // console.log(newobject['time'])
+
+// let obj1 = {
+//     name2:"ram",
+//     class:12,
+// }
+
+// // const obj3 = Object.assign({},obj1, newobject)
+// // console.log(obj3)
+
+// // const obj4= {...obj1,...newobject}
+// // console.log(obj4)
+
+// let newuser = [
+//     {
+//         Name:'suman',
+//         class:12
+//     },
+//     {
+//         userName:'suman',
+//         age:34
+//     },
+//     {
+//         Company:'varuna',
+//         day:'monday'
+//     },
+// ]
+
+// // console.log(Object.keys(newuser))
+// // console.log(Object.values(newuser))
+
+// let course ={
+//     CouseName:'Javascript',
+//     Price:345,
+//     couserInstructor:'surendra'
+// }
+
+// // console.log(course.couserInstructor)
+
+// const {Price:instractor} = course
+// // console.log(instractor)
+
+
+// // let newFun = function(...num){
+// //     console.log(num)
+// //         return num 
+       
+
+// // } 
+// // newFun(20, 30,4, 5)
+// let myFunction = (...num) => {
+//     console.log(num)
+//     return
+// };
+// myFunction(1,2,3,4)
+
+
+if(true){
+const userName = 'surendra'
+if(userName=='surendra'){
+    const two = 'Kumar'
+    console.log(`my Name is ${userName} ${two}`)
+
+}
+// console.log(two)
+
+console.log(userName)
+}
